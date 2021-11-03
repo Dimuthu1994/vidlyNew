1ed3e588d471f323a047cc066a08026278c14765
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-11-03 17:32:45 +0530

    handling form submit

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 function LoginForm(props) {
+  let handleSubmit = (e) => {
+    e.preventDefault();
+
+    //call the server
+    console.log("submitted");
+  };
+
   return (
     <div>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="username">Username</label>
           <input id="username" type="text" className="form-control" />
