b9eeef074aa6503e1eee81f359fc4210fe644992
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-11-03 12:23:27 +0530

    extracting method

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -42,19 +42,25 @@ function Movies(props) {
     setSortColumn(sortColumnClone);
   };
 
+  let getPagedData = () => {
+    const filteredMovies =
+      selectGenre && selectGenre._id
+        ? movies.filter((m) => m.genre._id === selectGenre._id)
+        : movies;
+
+    const sorted = _.orderBy(
+      filteredMovies,
+      [sortColumn.path],
+      [sortColumn.order]
+    );
+    const moviesPaginate = paginate(sorted, currentPage, pageSize);
+    return { totalCount: filteredMovies.length, data: moviesPaginate };
+  };
+
   if (movies.length === 0) return <p>There are zero movies in the database</p>;
 
-  const filteredMovies =
-    selectGenre && selectGenre._id
-      ? movies.filter((m) => m.genre._id === selectGenre._id)
-      : movies;
-
-  const sorted = _.orderBy(
-    filteredMovies,
-    [sortColumn.path],
-    [sortColumn.order]
-  );
-  const moviesPaginate = paginate(sorted, currentPage, pageSize);
+  const { totalCount, data } = getPagedData();
+  //
   return (
     <div className="row">
       <div className="col-3">
@@ -66,9 +72,9 @@ function Movies(props) {
       </div>
 
       <div className="col">
-        <p>Showing {sorted.length} in the database</p>
+        <p>Showing {totalCount} in the database</p>
         <MoviesTable
-          moviesPaginate={moviesPaginate}
+          moviesPaginate={data}
           onLike={handleLike}
           onDelete={handleDelete}
           onSort={handleSort}
@@ -76,7 +82,7 @@ function Movies(props) {
         />
 
         <Pagination
-          itemsCount={sorted.length}
+          itemsCount={totalCount}
           pageSize={4}
           onPageChange={handlePageChange}
           currentPage={currentPage}
