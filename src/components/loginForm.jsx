642bac0cc1d79743622cefdf347c90121ec07082
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-11-04 10:22:42 +0530

    validating form using JOI

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import Input from "./common/input";
+import Joi from "joi-browser";
 
 function LoginForm(props) {
   const [account, setAccount] = useState({ username: "", password: "" });
+  const [errors, setErrors] = useState({});
+  let schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
 
   let handleSubmit = (e) => {
     e.preventDefault();
     //call the server
+    const errorsNew = validate();
+    setErrors(errorsNew || {});
+
     console.log("submitted");
   };
 
@@ -14,6 +23,17 @@ function LoginForm(props) {
     const accountNew = { ...account };
     accountNew[input.name] = input.value;
     setAccount(accountNew);
+  };
+
+  let validate = () => {
+    const result = Joi.validate(account, schema, { abortEarly: false });
+
+    if (!result.error) return null;
+
+    const errors = {};
+    for (let item of result.error.details) errors[item.path[0]] = item.message;
+
+    return errors;
   };
 
   return (
@@ -25,6 +45,7 @@ function LoginForm(props) {
           label="Username"
           value={account.username}
           onChange={handleChange}
+          error={errors.username}
         />
 
         <Input
@@ -32,6 +53,7 @@ function LoginForm(props) {
           label="Password"
           value={account.password}
           onChange={handleChange}
+          error={errors.password}
         />
 
         <button className="btn btn-primary">Login</button>
