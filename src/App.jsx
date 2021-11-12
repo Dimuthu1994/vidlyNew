b72e79ea2b6f8a00651540427af5266ed090c255
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-11-12 12:08:53 +0530

    protecting routes

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,13 @@ function App() {
           <Route path="/login" component={LoginForm}></Route>
           <Route path="/logout" component={Logout}></Route>
           <Route path="/register" component={RegisterForm}></Route>
-          <Route path="/movies/:id" component={MovieForm}></Route>
+          <Route
+            path="/movies/:id"
+            render={(props) => {
+              if (!user) return <Redirect to="/login" />;
+              return <MovieForm {...props} />;
+            }}
+          ></Route>
           {/* <Route path="/movies/new" component={MovieForm}></Route> */}
           <Route
             path="/movies"
